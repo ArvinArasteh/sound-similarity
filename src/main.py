42a19dbb54a17f25e7cd7534b0a3029e17a71a86
Arvin Arasteh <dr.arvinarasteh@yahoo.com>
2021-04-10 22:19:06 -0700

    Fixed Main to work even better

--- src/main.py
+++ src/main.py
@@ -7,13 +7,13 @@ music2 = 'music/Darude_Sandstorm.wav'
 name1 = music1.replace("music/", "")
 name2 = music2.replace("music/", "")
 
-# # Tempo Diff
-# tempoDiff = diff(music1, music2)
-# print("Tempo difference between", name1, "and", name2, "is", tempoDiff)
+# Tempo Diff
+tempoDiff = diff(music1, music2)
+print("Tempo difference between", name1, "and", name2, "is", tempoDiff)
 
-# # Vocal Diff
-# vocalDiff = vocalDistanceBetweenTwoMusic(music1, music2)
-# print("Vocal difference between", name1, "and", name2, "is", vocalDiff)
+# Vocal Diff
+vocalDiff = vocalDistanceBetweenTwoMusic(music1, music2)
+print("Vocal difference between", name1, "and", name2, "is", vocalDiff)
 
 # Chorus Diff
 chorusDiff = compareChorus(music1, music2)
