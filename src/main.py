c3674486fde2df7c24dbef1e5fd26f7d49e9be48
Arvin Arasteh <dr.arvinarasteh@yahoo.com>
2021-04-18 19:05:45 -0700

    finished main and removed pi

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from vocals import vocalDistanceBetweenTwoMusic
 from tempo import diff
 from chorus import compareChorus
-
+ 
+# Compare All Files
 # MUSIC DATA SETS
 BASE_DIR = "../music/"
 music = ['Darude_Sandstorm.wav', 'America_A_Horse_With_No_Name.wav', 'Lift_Yourself_Kanye.wav', "The_Beatles_Don't_Let_Me_Down.wav"]
+
 # USE THE ONE BELOW FOR QUICK TESTING
 #music = ['Darude_Sandstorm.wav', 'America_A_Horse_With_No_Name.wav']
 
@@ -20,14 +22,28 @@ for music1 in music:
         vocalDiff = vocalDistanceBetweenTwoMusic(file1, file2)
 
         # Chorus Diff
-        #chorusDiff = compareChorus(file1, file2)
+        chorusDiff = compareChorus(file1, file2)
 
         print("Scores:")
         print("\tTempo:", tempoDiff)
         print("\tVocal:", vocalDiff)
-        #print("\tChorus:", chorusDiff)
+        print("\tChorus:", chorusDiff)
+
+        score = 0
+
+        if abs(tempoDiff) < 2:
+            score+=1
+        if abs(vocalDiff) < 3:
+            score+=1
+        if chorusDiff <= 20:
+            score+=1
+
+        print("\tScore:", score / 3)
+
+        if (score >= 0.5):
+            print("\tSimilarity:","Similar")
+        else:
+            print("\tSimilarity:","Not Similar")
+
         print("===========================================\n")
-
-
-
 
